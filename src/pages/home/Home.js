2582e201f84aa200e550d5fdd29855764f0fa19b
beqa <beqa.oqropiridze1994@gmail.com>
2022-12-17 15:40:47 +0400

    todat
    todat

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,25 +6,23 @@ import HelloWorld from "../../components/header/HelloWorld"
 import './home.css'
 
 export const Home = (props) => {
-    let showSomething = useState(false);
+    const [showSomething, setshowSomething] = useState(false);
 
     const renderContent = () => {
-        let content;
-
-        if ( showSomething ) {
-            content = <HelloWorld title="28" subTitle="subbTTitle" />
-        } else {
-            content = <Welcome user=" Mister" />
-        }
-        return content;
     };
 
-    return  <div className="col-12 my-3">
-        <button className="btn btn btn-primary" onClick={(event) => {
-            showSomething = !showSomething;
-            console.log('_ _showsomething_ _', showSomething);
-        }}> Do Something</button>
+    const handClick = () => {
+        console.log('_ _showsomething_ _', showSomething);
+        setshowSomething(!showSomething)
+    };
+
+
+    return ( <div className="col-12 my-3">
+        <button className="btn btn btn-primary" onClick={handClick}>  
+         Do Something
+         </button>
         <hr />
         {renderContent()}
-    </div>       
-}
+    </div>   
+    );    
+};
